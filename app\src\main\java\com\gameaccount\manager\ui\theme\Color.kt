b76d40a1f6b1dcb613d43379a667b76244d294d9
XACXF <328702091@qq.com>
2026-03-26 16:45:15 +0800

    Add file

---
+++ app\src\main\java\com\gameaccount\manager\ui\theme\Color.kt
@@ -0,0 +1,19 @@
+package com.gameaccount.manager.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// Custom colors
+val SkyBlue = Color(0xFF4facfe)
+val SkyPurple = Color(0xFF764ba2)
+val SkyPink = Color(0xFFec4899)
+val SkyGreen = Color(0xFF10B981)
+val SkyYellow = Color(0xFFF59E0B)
+val SkyRed = Color(0xFFEF4444)
